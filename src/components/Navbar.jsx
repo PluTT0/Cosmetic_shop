8d4b0d34f698b76b7c3756c482e9c95bf70ff650
Dmytro <kos.sok9205@gmail.com>
2022-09-11 22:01:40 +0200

    add footer (crate homepage)

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,92 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import { Badge } from '@mui/material';
+import ShoppingCartOutlined from '@mui/icons-material/ShoppingCartOutlined';
+import Search from '@mui/icons-material/Search';
+
+
+const Container = styled.div`
+    height: 60px;
+`;
+
+const Wrapper = styled.div`
+    padding: 10px 20px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`;
+
+const Left = styled.div`
+    flex: 1;
+    display: flex;
+    align-items: center;
+`;
+
+const Lanquage = styled.span`
+    font-size: 14px;
+    cursor: pointer;
+    display: flex;
+`;
+
+const SearchContainer = styled.div`
+    border: 0.5px solid lightgray;
+    display: flex;
+    align-content: center;
+    margin-left: 25px;
+    padding: 5px;
+`;
+
+const Input = styled.input`
+    border: none;
+    text-align: center;
+`;
+
+const Center = styled.div`
+    flex: 1;
+`;
+
+const Logo = styled.h1`
+    font-weight: bold;
+    text-align: center;
+`;
+
+const Right = styled.div`
+    flex: 1;
+    display: flex;
+    align-content: center;
+    justify-content: flex-end;
+`;
+
+const MenuItem = styled.div`
+    font-size: 14px;
+    cursor: pointer;
+    margin-left: 25px;
+`;
+
+const Navbar = () => {
+  return (
+    <Container>
+        <Wrapper>
+            <Left>
+                <Lanquage>EN</Lanquage>
+                <SearchContainer>
+                    <Input />
+                    <Search style={{color: "gray", fontSize: "16px" }} />
+                </SearchContainer>
+            </Left>
+            <Center><Logo>Corean Kosmetics</Logo></Center>
+            <Right>
+                <MenuItem>Register</MenuItem>
+                <MenuItem>SIGN IN</MenuItem>
+                <MenuItem>
+                <Badge badgeContent={4} color="primary">
+                    <ShoppingCartOutlined color="black" />
+                </Badge>
+                </MenuItem>
+            </Right>
+        </Wrapper>
+    </Container>
+  )
+}
+
+export default Navbar
---
+++ src/data.js
@@ -0,0 +1,83 @@
+export const sliderItems = [
+    {
+      id: 1,
+      img: "https://images.pexels.com/photos/5632324/pexels-photo-5632324.jpeg?cs=srgb&dl=pexels-karolina-grabowska-5632324.jpg&fm=jpg",
+      title: "SUMMER SALE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "DBE9E9",
+    },
+    {
+      id: 2,
+      img: "https://images.pexels.com/photos/5632393/pexels-photo-5632393.jpeg?cs=srgb&dl=pexels-karolina-grabowska-5632393.jpg&fm=jpg",
+      title: "AUTUMN COLLECTION",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "ECEEED",
+    },
+    {
+      id: 3,
+      img: "https://i.ibb.co/cXFnLLV/3.png",
+      title: "LOUNGEWEAR LOVE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fbf0f4",
+    },
+  ];
+
+export const categories = [{
+    id: 1,
+    img: "https://res.cloudinary.com/dlw1urjvs/image/upload/v1655116351/cosmetic%20shop/photo_2022-05-26_23-38-52_iwg3m7.jpg",
+    title: "SPA COSMETICS",
+  },
+  {
+    id: 2,
+    img: "https://res.cloudinary.com/dlw1urjvs/image/upload/v1655116351/cosmetic%20shop/photo_2022-05-26_23-39-01_fglky3.jpg",
+    title: "MILK FOR BODY",
+  },
+  {
+    id: 3,
+    img: "https://res.cloudinary.com/dlw1urjvs/image/upload/v1655116352/cosmetic%20shop/photo_2022-06-12_13-01-35_vrtlg0.jpg",
+    title: "AROMA SOAP",
+  },
+];
+
+export const popularProducts = [
+  {
+    id:1,
+    img:"https://res.cloudinary.com/dlw1urjvs/image/upload/v1655116352/cosmetic%20shop/photo_2022-06-12_13-01-29_tfn1hb.jpg",
+  },
+  {
+    id:2,
+    img:"https://res.cloudinary.com/dlw1urjvs/image/upload/v1655116352/cosmetic%20shop/photo_2022-06-12_13-01-32_opt6qe.jpg",
+  },
+  {
+    id:3,
+    img:"https://res.cloudinary.com/dlw1urjvs/image/upload/v1655116351/cosmetic%20shop/photo_2022-05-26_23-38-57_ix8xoj.jpg",
+  },
+  {
+    id:4,
+    img:"https://res.cloudinary.com/dlw1urjvs/image/upload/v1655116351/cosmetic%20shop/photo_2022-05-26_23-39-01_fglky3.jpg",
+  },
+  {
+    id:5,
+    img:"https://res.cloudinary.com/dlw1urjvs/image/upload/v1655116351/cosmetic%20shop/photo_2022-05-26_23-38-45_rr98so.jpg",
+  },
+  {
+    id:6,
+    img:"https://res.cloudinary.com/dlw1urjvs/image/upload/v1655116352/cosmetic%20shop/photo_2022-06-12_13-01-44_pe9mjf.jpg",
+  },
+  {
+    id:7,
+    img:"https://res.cloudinary.com/dlw1urjvs/image/upload/v1655116352/cosmetic%20shop/photo_2022-06-12_13-01-39_ke6hnm.jpg",
+  },
+  {
+    id:8,
+    img:"https://res.cloudinary.com/dlw1urjvs/image/upload/v1650943109/cosmetic%20shop/pexels-pnw-production-8490089_odkhdq.jpg",
+  },
+  {
+    id:9,
+    img:"https://res.cloudinary.com/dlw1urjvs/image/upload/v1650943109/cosmetic%20shop/pexels-pnw-production-8490089_odkhdq.jpg",
+  },
+  {
+    id:10,
+    img:"https://res.cloudinary.com/dlw1urjvs/image/upload/v1650943109/cosmetic%20shop/pexels-pnw-production-8490089_odkhdq.jpg",
+  },
+]
